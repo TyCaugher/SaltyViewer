4daaf7ae6879a3056de17598b59eac0f9b72fb80
Tyler Kauffman <tyler.kauffman@cognizant.com>
2022-05-10 17:08:02 -0500

    Basic app skeleton

--- app/src/main/java/com/example/saltyviewer/MainActivity.kt
+++ app/src/main/java/com/example/saltyviewer/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.saltyviewer.ui.theme.SaltyViewerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AppLayout()
                 }
             }
         }
@@ -30,14 +32,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AppLayout() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "SaltyViewer")
+                },
+                actions = {
+                    // Icons and stuff here
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp))
+    }
+}
+
+@Composable
+fun BodyContent(padding: Modifier) {
+
+}
+
+
+@Composable
+fun GridImageList() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun AppLayoutPreview() {
     SaltyViewerTheme {
-        Greeting("Android")
+        AppLayout()
     }
 }
---
+++ app/src/main/java/com/example/saltyviewer/api/ApiEndpointInterface.kt
@@ -0,0 +1,8 @@
+package com.example.saltyviewer.api
+
+import retrofit2.http.GET
+
+public interface ApiEndpointInterface {
+    //
+
+}
