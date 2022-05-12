65155dc832e97be3218693c473d484b9d51ade0d
Kroren <tak4506@gmail.com>
2022-05-12 13:41:32 -0500

    Initial WIP on pagination

--- app/src/main/java/com/example/saltyviewer/api/Posts.kt
+++ app/src/main/java/com/example/saltyviewer/api/Posts.kt
@@ -1,5 +1,6 @@
 package com.example.saltyviewer.api
 
 data class Posts(
+    val page: Int,
     val posts: List<Post>
 )
--- app/src/main/java/com/example/saltyviewer/api/e6Api.kt
+++ app/src/main/java/com/example/saltyviewer/api/e6Api.kt
@@ -3,11 +3,12 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface e6Api {
 
     // Search format: posts.json? tags=order:score+dragon+rating:s &limit=5
     @Headers("User-Agent: SaltyViewer/0.1 (By Kroren)")
     @GET("/posts.json?tags=order:score+dragon+rating:s&limit=10")
-    suspend fun getPosts(): Response<Posts>
+    suspend fun getPosts(@Query("tags") tags: List<String>?, @Query("page") page: Int): Response<Posts>
 }
